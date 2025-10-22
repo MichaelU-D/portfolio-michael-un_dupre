a67880afca9c5bd1398dc148cddc42042e94ddd5
Honmasu <112128372+Honmasu@users.noreply.github.com>
2025-10-22 19:04:31 -0400

    changement de page et animation gsap
    j'avais fait l'animation en css, dans ce commit j'ai fait l'anim avec gsap

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
     body.classList.toggle('dark-mode');
   });
 });
+
+// anime gsap
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollLogo = document.querySelector(".scroll");
+
+  gsap.to(scrollLogo, {
+    y: 15,
+    duration: 1,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+  });
+});
